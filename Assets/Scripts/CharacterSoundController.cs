a1d10d0e16136432747d00d2d5dbbc903656b08d
Naufal Shidqi <naufalshidqi99@gmail.com>
2021-10-27 01:39:51 +0700

    First Build

---
+++ Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+    public AudioClip scoreHighlight;
+
+    private AudioSource audioPlayer;
+    private CharacterSoundController sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+}
